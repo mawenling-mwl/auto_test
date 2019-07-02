36da86ae544dfd0dd1eebb7f51611106ba6f9b19
mawenling-mwl <mawenling@gaosiedu.com>
2019-07-02 19:26:00 +0800

    add src

---
+++ src/main/java/com/course/testng/BasicAnnotation.java
@@ -0,0 +1,11 @@
+package com.course.testng;
+
+
+import org.testng.annotations.Test;
+
+public class BasicAnnotation {
+    @Test
+    public void testCase1(){
+        System.out.println("这是第一个测试用例");
+    }
+}
